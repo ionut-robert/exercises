4bad7f78f10ce1185d2277569d9f797be0d8197e
Robert <ionutrobertratoiu@gmail.com>
2026-06-19 23:59:05 +0300

    Add files via upload

---
+++ 10. **All Sundays of a Year.py
@@ -0,0 +1,23 @@
+#10. All Sundays of a Year.py
+import datetime as dt
+
+Year = int(input('Year: '))#2026
+
+first_day = dt.datetime.strptime(f'{Year}-01-01','%Y-%m-%d').date()
+
+for d in range(0,7):
+    x = first_day + dt.timedelta(d)
+
+    if x.weekday() == 6:
+        first_Sunday = x
+
+All_Sundays = [first_Sunday]
+
+print(All_Sundays)
+while All_Sundays[-1].year < Year+1:
+    All_Sundays.append(All_Sundays[-1]+dt.timedelta(7))
+
+All_Sundays.pop()
+
+for i in All_Sundays:
+    print(i,i.weekday())
