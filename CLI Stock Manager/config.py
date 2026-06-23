6f63aa3568394832478624e2b58df421783b6599
Robert <ionutrobertratoiu@gmail.com>
2026-06-23 03:18:42 +0300

    Update

--- CLI Stock Manager/config.py
+++ CLI Stock Manager/config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from os import getenv
+from urllib.parse import quote_plus
 
 load_dotenv()
 
@@ -7,4 +8,4 @@ DB_DRIVER=getenv("DB_DRIVER")
 SERVER =getenv("SERVER")
 DB_DATABASE=getenv("DB_DATABASE")
 DB_USER=getenv("DB_USER")
-DB_PASSWORD=getenv("DB_PASSWORD")
+DB_PASSWORD=quote_plus(getenv("DB_PASSWORD"))
